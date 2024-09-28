8494002f411a99c5b8d87b790158a1067ef0d7fc
ahumblecat <ayman.abid@student.ehb.be>
2024-09-28 15:06:06 +0200

    done

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,18 @@
-"use strict";
+signature();
 
-alert("Draw your space invader here");
+function signature() {
+	let canvas = document.querySelector("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let context = canvas.getContext("2d");
+
+	context.beginPath();
+	context.fillRect(50, 50, 300, 300); //square
+	context.fillStyle = "lightgreen";
+	context.fillRect(175, 75, 50, 50); //top of signature
+	context.fillRect(125, 125, 50, 200); //left side of signature
+	context.fillRect(225, 125, 50, 200); //left side of signature
+	context.fillRect(175, 175, 50, 50); //middle of signature
+}
+
+//klaar
